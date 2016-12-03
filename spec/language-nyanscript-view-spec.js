b00b3b2ff82d1f4d96988112baa2ce42a0d33811
kiochan <sunxingchen@live.com>
2016-12-03 09:29:34 +0100

    package init && first commit

---
+++ spec/language-nyanscript-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageNyanscriptView from '../lib/language-nyanscript-view';
+
+describe('LanguageNyanscriptView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
